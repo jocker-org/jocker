35ba21f4d83e65c3cdbb8afd049f60842ca10b01
heph <srht@mrkeebs.eu>
2024-11-24 17:12:05 +0100

    feat(parser): add more commands
    drafting some commands

--- internal/parser/jockerfile.go
+++ internal/parser/jockerfile.go
@@ -5,8 +5,14 @@ import (
 )
 
 type Jockerfile struct {
-	Image string `json:"image"`
-	Copy  string `json:"copy"`
+	Image      string   `json:"image"`      // Base image
+	Copy       []string `json:"copy"`       // Copy source:destination pairs
+	Run        []string `json:"run"`        // Commands to execute during image build
+	Cmd        []string `json:"cmd"`        // Command to run when container starts
+	WorkDir    string   `json:"workdir"`    // Working directory inside the container
+	Expose     []int    `json:"expose"`     // Ports to expose
+	EntryPoint []string `json:"entrypoint"` // Entrypoint command
+	Env        map[string]string `json:"env"` // Environment variables
 }
 
 func ParseJockerfile(jsonStr string) (*Jockerfile, error) {
