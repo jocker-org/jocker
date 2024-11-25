4da5cdb1f99e20044637e165b4af175c6f88e4e7
heph <srht@mrkeebs.eu>
2024-11-25 12:18:27 +0100

    feat(devenv): add build alias

--- devenv.nix
+++ devenv.nix
@@ -21,6 +21,10 @@
     go run main.go debug-dump | buildctl debug dump-llb | jq
   '';
 
+  scripts.run-build.exec = ''
+    go run main.go debug-dump | buildctl build --local context=. --output type=image,name=docker.io/jocker/test,push=false
+  '';
+
   enterShell = ''
     hello
     git --version
