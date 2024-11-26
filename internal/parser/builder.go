f7fee57b6f9fe3559de6ec1bcadcb3019cc007ee
Omar Polo <op@omarpolo.com>
2024-11-26 15:34:45 +0000

    actually load the jockerfile from the context

--- internal/parser/builder.go
+++ internal/parser/builder.go
@@ -7,14 +7,60 @@ import (
 	"log"
 
 	"github.com/containerd/platforms"
+	"github.com/google/go-jsonnet"
 	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/exporter/containerimage/exptypes"
 	"github.com/moby/buildkit/frontend/gateway/client"
 	specs "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
+func GetJockerfile(ctx context.Context, c client.Client) (string, error) {
+	opts := c.BuildOpts().Opts
+	filename := opts["filename"]
+	if filename == "" {
+		filename = "Jockerfile"
+	}
+
+	src := llb.Local("context",
+		llb.IncludePatterns([]string{filename}),
+		llb.SessionID(c.BuildOpts().SessionID),
+		llb.SharedKeyHint("Jockerfile"),
+	)
+
+	def, err := src.Marshal(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal local source: %w", err)
+	}
+
+	res, err := c.Solve(ctx, client.SolveRequest{
+		Definition: def.ToPB(),
+	})
+	if err != nil {
+		return "", err
+	}
+
+	ref, err := res.SingleRef()
+	if err != nil {
+		return "", err
+	}
+
+	jockerfile, err := ref.ReadFile(ctx, client.ReadRequest{
+		Filename: filename,
+	})
+	if err != nil {
+		return "", err
+	}
+	return string(jockerfile), nil
+}
+
 func Build(ctx context.Context, c client.Client) (*client.Result, error) {
-	jsonStr, err := EvaluateJsonnetFile("Jockerfile")
+	jockerfile, err := GetJockerfile(ctx, c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	vm := jsonnet.MakeVM()
+	jsonStr, err := vm.EvaluateAnonymousSnippet("jockerfile", jockerfile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -43,23 +89,19 @@ func Build(ctx context.Context, c client.Client) (*client.Result, error) {
 		return nil, err
 	}
 
-	imgConfig := specs.ImageConfig{
-		Cmd: []string {"ls"},
-	}
-
+	p := platforms.DefaultSpec()
 	img := &specs.Image{
-		Platform: specs.Platform{
-			Architecture: "amd64",
-			OS:           "linux",
+		Platform: p,
+		Config: specs.ImageConfig{
+			Cmd: []string {"ls"},
 		},
-		Config: imgConfig,
 	}
 
 	config, err := json.Marshal(img)
 	if err != nil {
 		return nil, fmt.Errorf("failed to marshal image config: %w", err)
 	}
-	k := platforms.Format(platforms.DefaultSpec())
+	k := platforms.Format(p)
 
 	res.AddMeta(fmt.Sprintf("%s/%s", exptypes.ExporterImageConfigKey, k), config)
 	res.SetRef(ref)
