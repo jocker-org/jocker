bce9c67bcb2b27953a169f7414c69f710a4f76f0
heph <srht@mrkeebs.eu>
2024-11-27 10:23:58 +0100

    chore: devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,22 +1,10 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/basics/
   env.BUILDKIT_HOST = "docker-container://buildkit";
-
-  # https://devenv.sh/packages/
   packages = with pkgs; [ git gopls go buildkit jq mkdocs dive ];
-
-  # https://devenv.sh/languages/
   languages.go.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
   scripts.run-debug.exec = ''
     go run main.go debug-dump | buildctl debug dump-llb | jq
   '';
@@ -26,28 +14,6 @@
   '';
 
   scripts.run-publish.exec = ''
-    go run main.go debug-dump | buildctl build --local context=. --output type=image,name=ghcr.io/heph2/jocker-test:latest,push=true
+    docker build -t ghcr.io/jocker-org/jocker:latest -f Dockerfile . && docker push ghcr.io/jocker-org/jocker:latest
   '';
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
