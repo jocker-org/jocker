8136dba2608e32b6fae94ab578d01a6a96b8a279
heph <srht@mrkeebs.eu>
2025-04-24 14:53:11 +0200

    handle debub-dump and build image resolving
    using different resolvers for debug-dump allow us to resolve the image
    metadata both for debug-dump and docker build

--- internal/parser/llb.go
+++ internal/parser/llb.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"context"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -10,7 +11,9 @@ import (
 	"github.com/moby/buildkit/client/llb"
 	"github.com/moby/buildkit/client/llb/imagemetaresolver"
 	"github.com/moby/buildkit/client/llb/sourceresolver"
-	// specs "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/moby/buildkit/frontend/gateway/client"
+
+	gw "github.com/moby/buildkit/frontend/gateway/client"
 	dockerspec "github.com/moby/docker-image-spec/specs-go/v1"
 )
 
@@ -20,7 +23,6 @@ type BuildContext struct {
 	context llb.State
 	debug   string
 	ctx     context.Context
-	// image   specs.ImageConfig
 	image dockerspec.DockerOCIImage
 }
 
@@ -38,11 +40,35 @@ func parseKeyValue(env string) (string, string) {
 	return parts[0], v
 }
 
+// normalizeImage returns the image with the docker.io/library prefixed if it's not
+// from a registry. Otherwise we can't resolve the Image Metadata
+func normalizeImage(image string) string {
+	parts := strings.Split(image, "/")
+
+	if len(parts) > 1 && (strings.Contains(parts[0], ".") || strings.Contains(parts[0], ":")) {
+		return image
+	}
+
+	if len(parts) == 1 {
+		return "docker.io/library/" + image
+	}
+
+	return "docker.io/" + image
+}
+
 // debugLog conditionally logs a debug message and injects an echo command into the LLB state.
+// it also adds a random uuid for invalidating docker cache
+// https://github.com/docker/buildx/issues/2387
 func debugLog(b *BuildContext, msg string) {
+	by := make([]byte, 4)
+	_, err := rand.Read(by)
+	if err != nil {
+		slog.Error("error while creating unique id", "error", err)
+	}
+	uuid := fmt.Sprintf("%x ", by)
 	if b.debug == "all" {
 		slog.Warn("Step: ", "buildctx", msg)
-		b.state = b.state.Run(shf("echo DEBUG " + msg)).Root()
+		b.state = b.state.Run(shf("echo DEBUG " + uuid + msg)).Root()
 	}
 }
 
@@ -99,47 +125,51 @@ func shf(cmd string, v ...interface{}) llb.RunOption {
 	return llb.Args([]string{"/bin/sh", "-c", fmt.Sprintf(cmd, v...)})
 }
 
-func (stage *BuildStage) ToLLB(b *BuildContext) llb.State {
+func (stage *BuildStage) ToLLB(b *BuildContext, c client.Client) llb.State {
 	if stage.From == "scratch" {
 		b.state = llb.Scratch()
 	} else {
-		metaresolver := imagemetaresolver.Default()
-		_, _, dt, err := metaresolver.ResolveImageConfig(b.ctx, "docker.io/library/" + stage.From, sourceresolver.Opt{
-			ImageOpt: &sourceresolver.ResolveImageOpt{
-				ResolveMode: llb.ResolveModeDefault.String(),
-			},
-		})
-
-		if err != nil {
-			debugLog(b, "failed to resolve image")
-			slog.Error("failed to resolve image", "FROM", err)
-		}
-
-		// var img specs.ImageConfig
 		var img dockerspec.DockerOCIImage
-		if err := json.Unmarshal(dt, &img); err != nil {
-			debugLog(b, "failed to unmarshal image")
-			slog.Error("failed to unmarshal image", "FROM", err)
+		baseImg := normalizeImage(stage.From)
+		if c == nil {
+			metaresolver := imagemetaresolver.Default()
+			_, _, dt, err := metaresolver.ResolveImageConfig(b.ctx, baseImg, sourceresolver.Opt{
+				ImageOpt: &sourceresolver.ResolveImageOpt{
+					ResolveMode: llb.ResolveModeDefault.String(),
+				},
+			})
+			if err != nil {
+				debugLog(b, "failed to resolve image")
+				slog.Error("failed to resolve image", "FROM", err)
+			}
+			if err := json.Unmarshal(dt, &img); err != nil {
+				debugLog(b, "failed to unmarshal image")
+				slog.Error("failed to unmarshal image", "FROM", err)
+			}
+		} else {
+			_, _, dt, err := gw.Client.ResolveImageConfig(c, b.ctx, baseImg, sourceresolver.Opt{
+				ImageOpt: &sourceresolver.ResolveImageOpt{
+					ResolveMode: llb.ResolveModeDefault.String(),
+				},
+			})
+			if err != nil {
+				debugLog(b, "failed to resolve image")
+				slog.Error("failed to resolve image", "FROM", err)
+			}
+			if err := json.Unmarshal(dt, &img); err != nil {
+				debugLog(b, "failed to unmarshal image")
+				slog.Error("failed to unmarshal image", "FROM", err)
+			}
+
 		}
-
-		debugLog(b, string(dt))
-
 		b.state = llb.Image(stage.From)
-
 		b.image = img
 
-		debugLog(b, b.image.Config.WorkingDir)
-		debugLog(b, img.Config.WorkingDir)
-		// This checks that the envs are set
+		// initialize metadata
 		for _, env := range img.Config.Env {
 			debugLog(b, env)
 			k, v := parseKeyValue(env)
 			b.state = b.state.AddEnv(k, v)
-		}
-
-		// b.state, err = llb.Image(stage.From).WithImageConfig(dt)
-		if err != nil {
-			slog.Error("failed to insert config in image", "FROM", err)
 		}
 	}
 
@@ -151,7 +181,7 @@ func (stage *BuildStage) ToLLB(b *BuildContext) llb.State {
 	return b.state
 }
 
-func (j *Jockerfile) ToLLB(debug string, ctx context.Context) llb.State {
+func (j *Jockerfile) ToLLB(debug string, ctx context.Context, c client.Client) llb.State {
 	b := BuildContext{
 		stages: make(map[string]llb.State),
 		debug:  debug,
@@ -166,7 +196,7 @@ func (j *Jockerfile) ToLLB(debug string, ctx context.Context) llb.State {
 
 	for _, stage := range j.Stages {
 		slog.Info("Building stage", "ctx", stage.Name)
-		state = stage.ToLLB(&b)
+		state = stage.ToLLB(&b, c)
 		b.stages[stage.Name] = state
 	}
 
@@ -174,8 +204,6 @@ func (j *Jockerfile) ToLLB(debug string, ctx context.Context) llb.State {
 	slog.Info("setting image config")
 	slog.Info(b.image.Config.WorkingDir)
 	j.Image = b.image
-
-	slog.Info(j.Image.Config.WorkingDir)
 
 	return state
 }
